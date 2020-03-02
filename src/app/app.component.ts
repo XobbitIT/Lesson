ed7031e17d114d2a182dfc9f2a16b9976436636a
Lesson2 <romann34@gmail.com>
2020-03-02 20:56:23 +0200

    lesson1

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'corse';
+  counter = 1;
+  stateDropDown = false
+  products = [
+    {
+      id: 1,
+      name: 'product 1',
+      price: 36
+    },
+    {
+      id: 2,
+      name: 'product 2',
+      price: 46
+    },
+    {
+      id: 3,
+      name: 'product 3',
+      price: 31
+    }
+  ]
+  changeTitle() {
+    this.title = 'Lesson';
+  
+
+    }
+    incrementCounter(){
+      this.counter++; 
+    }
+    toggleDropDown() {
+      this.stateDropDown = !this.stateDropDown;
+    }
+ }
+
